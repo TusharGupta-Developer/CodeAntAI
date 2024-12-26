da12ebdfe0f446831b96454e83824ebf53106579
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-12-27 00:00:08 +0530

    App.js update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Repositories from "./pages/Home/repositories";
+import Repositories from "./pages/Home/Repositories.jsx";
 import SignIn from "./pages/SignIn/SignIn";
 
 const App = () => {
